31ab75a04e609e1e0b1e93bfd0fbec1a815d5c71
GGlebux <rejngleb@gmail.com>
2025-07-18 19:41:12 +0400

    clear temp files & add README.md

--- src/main/java/project/assay/AssayApplication.java
+++ src/main/java/project/assay/AssayApplication.java
@@ -15,18 +15,6 @@ public class AssayApplication {
 		SpringApplication.run(AssayApplication.class, args);
 	}
 
-//	@Bean
-//	JedisConnectionFactory jedisConnectionFactory() {
-//		return new JedisConnectionFactory();
-//	}
-//
-//	@Bean
-//	public RedisTemplate<String, Object> redisTemplate() {
-//		RedisTemplate<String, Object> template = new RedisTemplate<>();
-//		template.setConnectionFactory(jedisConnectionFactory());
-//		template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
-//		return template;
-//	}
 
 	@Bean
 	@Scope(SCOPE_PROTOTYPE)
